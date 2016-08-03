ccf3b1e4cd08e6c8875f3b76a5426ca7ea12c2a1
Brian0610 <brian49345@gmail.com>
2016-08-02 20:10:12 -0400

    x

--- js/app.js
+++ js/app.js
@@ -1,38 +1,37 @@
 $(function(){
-
 	// create function loadData that...
 	function loadData(linkID) {		
 		//...loaads JSON data onto page
-		$.getJSON('data/data.json')
+		$.getJSON('../data/data.json')
 			.done( function(data) {
 				alert("working");
 				// create msg variable to store HTML string
 				var msg = ""; 
 				// use Switch statement to find required data, add markup to msg 
-					switch(linkID) {
-						case "beer":
-							msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.beer.perCap +'<br>Market Share: ' + drinks.beer.share + '</p></div>'; 
+				switch(linkID) {
+					case "beer":
+						msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.beer.perCap +'<br>Market Share: ' + drinks.beer.share + '</p></div>'; 
+						break; 
+					case "wine":
+						msg += '<div class="drink"><h2 class="drink">Wine</h2><p>Annual Consumption, Per Capita: ' + drinks.wine.perCap +'<br>Market Share: ' + drinks.wine.share + '</p></div>'; 
 							break; 
-						case "wine":
-							msg += '<div class="drink"><h2 class="drink">Wine</h2><p>Annual Consumption, Per Capita: ' + drinks.wine.perCap +'<br>Market Share: ' + drinks.wine.share + '</p></div>'; 
- 							break; 
-						case "sprits":
-							msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.spirits.perCap +'<br>Market Share: ' + drinks.spirits.share + '</p></div>'; 
-							break; 
-						case "coffee":
-							msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.coffee.perCap +'<br>Market Share: ' + drinks.coffee.share + '</p></div>'; 
-							break; 
-						case "tea":
-							msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.tea.perCap +'<br>Market Share: ' + drinks.tea.share + '</p></div>'; 
-							break; 
-						case "cola":
-							msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.softDrinks.perCap +'<br>Market Share: ' + drinks.softDrinks.share + '</p></div>'; 
-							break;
-						case "bottledH20":
-							msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.bottledH20.perCap +'<br>Market Share: ' + drinks.bottledH20.share + '</p></div>'; 
-							break;
-					}
-					$("#content").html(msg); 
+					case "sprits":
+						msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.spirits.perCap +'<br>Market Share: ' + drinks.spirits.share + '</p></div>'; 
+						break; 
+					case "coffee":
+						msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.coffee.perCap +'<br>Market Share: ' + drinks.coffee.share + '</p></div>'; 
+						break; 
+					case "tea":
+						msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.tea.perCap +'<br>Market Share: ' + drinks.tea.share + '</p></div>'; 
+						break; 
+					case "cola":
+						msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.softDrinks.perCap +'<br>Market Share: ' + drinks.softDrinks.share + '</p></div>'; 
+						break;
+					case "bottledH20":
+						msg += '<div class="drink"><h2 class="drink">Beer</h2><p>Annual Consumption, Per Capita: ' + drinks.bottledH20.perCap +'<br>Market Share: ' + drinks.bottledH20.share + '</p></div>'; 
+						break;
+				}
+				$("#content").html(msg); 
 			}).fail( function() {
 				alert("not working"); 
 			}); 
