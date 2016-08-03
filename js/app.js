5f3b294657c4cf972ad1757420f2f68b28200f18
Brian0610 <brian49345@gmail.com>
2016-08-02 21:03:35 -0400

    x

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(function(){
 		$.getJSON('data/data.json')
 			.done( function(data) {
 				var $data = data;
-				alert($data);
+				alert($data.drinks[0].name);
 				// create msg variable to store HTML string
 				var msg = ""; 
 				// use Switch statement to find required data, add markup to msg 
